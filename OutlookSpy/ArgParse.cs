990d4f7a42fcd34c570cd4dfc7bea3e431613e93
Anthony <acole76@gmail.com>
2020-07-21 13:43:31 -0500

    Add project files.

---
+++ OutlookSpy/ArgParse.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace ArgumentParser
+{
+  public class ArgParse
+  {
+    Dictionary<string, string> argumentDict = new Dictionary<string, string>();
+    List<ArgItem> argumentList = new List<ArgItem>();
+
+    public enum ArgParseType
+    {
+      Long = 0,
+      Int = 1,
+      DateTime = 2,
+      String = 3,
+      Boolean = 4,
+      File = 5,
+      Pid = 6, //process id
+      Url = 7,
+      Choice = 8
+    };
+
+    public ArgParse(params ArgItem[] _argClass)
+    {
+      foreach (ArgItem arg in _argClass)
+      {
+        argumentList.Add(arg);
+      }
+    }
+
+    public ArgParse(List<ArgItem> _argList)
+    {
+      argumentList = _argList;
+    }
+
+    public void parse(string[] args)
+    {
+      string prev = null;
+      foreach (string arg in args)
+      {
+        if (arg == "-h" || arg == "--help")
+        {
+          showHelp("");
+          return;
+        }
+
+        if (arg.StartsWith("--") || arg.StartsWith("-"))
+        {
+          ArgItem item = getArgItemBySwitch(arg);
+
+          if (item == null)
+          {
+            showHelp(string.Format("Unknown switch: {0}", arg));
+            return;
+          }
+
+          prev = item.longName;
+          if (argumentDict.ContainsKey(item.longName))
+          {
+            showHelp(string.Format("duplicate switch: {0}", item.longName));
+            return;
+          }
+
+          // set the initial value to the default value specified
+          argumentDict.Add(item.longName, item.defaultValue);
+        }
+        else
+        {
+          if (prev != null) // prev will be null on the first iteration of this loop
+          {
+            argumentDict[prev] = arg;
+          }
+        }
+      }
+
+      // sets the defaults if a value does not exist
+      foreach (ArgItem argClass in argumentList)
+      {
+        if (argClass.defaultValue != null && argClass.defaultValue.Length > 0)
+        {
+          if (!argumentDict.ContainsKey(argClass.longName))
+          {
+            argumentDict.Add(argClass.longName, argClass.defaultValue);
+          }
+        }
+      }
+
+      // check is required
+      foreach (ArgItem argClass in argumentList)
+      {
+        if (argClass.required)
+        {
+          if (!argumentDict.ContainsKey(argClass.longName))
+          {
+            showHelp(string.Format("Field {0} is required but is missing.", argClass.longName));
+            return;
+          }
+        }
+      }
+
+      // check is valid type
+      foreach (ArgItem argClass in argumentList)
+      {
+        if (argumentDict.ContainsKey(argClass.longName))
+        {
+          string dataType = argClass.dataType.ToString();
+          string dataValue = argumentDict[argClass.longName];
+
+          if (argClass.required || dataValue.Length > 0)
+          {
+            try
+            {
+              switch (argClass.argType)
+              {
+                case ArgParseType.Int:
+                  Int32.Parse(dataValue);
+                  break;
+                case ArgParseType.Long:
+                  Int64.Parse(dataValue);
+                  break;
+                case ArgParseType.DateTime:
+                  DateTime.Parse(dataValue);
+                  break;
+                case ArgParseType.Boolean:
+                  bool.Parse(dataValue);
+                  break;
+                case ArgParseType.File:
+                  if (!File.Exists(dataValue))
+                  {
+                    throw new Exception("File does not exist");
+                  }
+                  break;
+                case ArgParseType.Pid:
+                  Int32 pid = Int32.Parse(dataValue);
+                  Process proc = Process.GetProcessById(pid);
+                  if (proc == null)
+                  {
+                    throw new Exception("Process does not exist");
+                  }
+                  break;
+                case ArgParseType.Url:
+                  try
+                  {
+                    Uri u = null;
+                    Uri.TryCreate(dataValue, UriKind.Absolute, out u);
+                    if (u == null)
+                    {
+                      throw new Exception("Invalid URL.");
+                    }
+                  }
+                  catch (Exception)
+                  {
+                    throw new Exception("Invalid URL.");
+                  }
+                  break;
+                case ArgParseType.Choice:
+                  int foundAt = Array.IndexOf(argClass.validValues, dataValue);
+                  if (foundAt == -1)
+                  {
+                    string validCsv = string.Join(",", argClass.validValues);
+                    throw new Exception(string.Format("Invalid option.  The only valid options are : {0}", validCsv));
+                  }
+                  break;
+                default:
+                  break;
+              }
+            }
+            catch (Exception e)
+            {
+              showHelp(string.Format("{0} - {1}", argClass.longName, e.Message));
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    public void showHelp(string msg)
+    {
+      bool hasOptional = false;
+      if (msg.Length > 0)
+      {
+        Console.WriteLine("Error: {0}\n", msg);
+      }
+
+      Console.WriteLine("Usage: {0} <options>", AppDomain.CurrentDomain.FriendlyName);
+      Console.WriteLine("\tRequired");
+      foreach (ArgItem argClass in argumentList)
+      {
+        if (argClass.required == true)
+        {
+          Console.WriteLine("\t\t-{0}, --{1}\t{2}", argClass.shortName, argClass.longName, argClass.helpText);
+        }
+        else
+        {
+          hasOptional = true;
+        }
+      }
+
+      if (hasOptional)
+      {
+        Console.WriteLine("\n\tOptional");
+        foreach (ArgItem argClass in argumentList)
+        {
+          if (argClass.required == false)
+          {
+            Console.WriteLine("\t\t-{0}, --{1}\t{2}", argClass.shortName, argClass.longName, argClass.helpText);
+          }
+        }
+      }
+
+      if (Debugger.IsAttached)
+      {
+        Console.ReadLine();
+      }
+
+      Environment.Exit(0);
+    }
+
+    private ArgItem getArgItemBySwitch(string arg)
+    {
+      if (arg.StartsWith("--"))
+      {
+        foreach (ArgItem item in argumentList)
+        {
+          if (item.longName == arg.Substring(2))
+          {
+            return item;
+          }
+        }
+      }
+      else
+      {
+        foreach (ArgItem item in argumentList)
+        {
+          if (item.shortName == arg.Substring(1))
+          {
+            return item;
+          }
+        }
+      }
+
+      return null;
+    }
+
+    public T Get<T>(string key)
+    {
+      if (!argumentDict.ContainsKey(key))
+        return default(T);
+
+      try
+      {
+        return (T)Convert.ChangeType(argumentDict[key], typeof(T));
+      }
+      catch (Exception)
+      {
+        return default(T);
+      }
+    }
+  }
+
+  public class ArgItem
+  {
+    public string longName { get; set; } //double dash option --domain
+    public string shortName { get; set; } //single dash option -d
+    public bool required { get; set; }
+    public string helpText { get; set; }
+    public string defaultValue { get; set; }
+    public Type dataType { get; set; }
+    public ArgParse.ArgParseType argType { get; set; }
+    public string[] validValues { get; set; }
+
+    public ArgItem(string _longName, string _shortName, bool _required)
+    {
+      init(_longName, _shortName, _required, "", "", ArgParse.ArgParseType.String, new string[] { });
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText)
+    {
+      init(_longName, _shortName, _required, _helpText, "", ArgParse.ArgParseType.String, new string[] { });
+    }
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText, string _defaultValue)
+    {
+      init(_longName, _shortName, _required, _helpText, _defaultValue, ArgParse.ArgParseType.String, new string[] { });
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, ArgParse.ArgParseType _dataType)
+    {
+      init(_longName, _shortName, _required, "", "", _dataType, new string[] { });
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText, ArgParse.ArgParseType _dataType)
+    {
+      init(_longName, _shortName, _required, _helpText, "", _dataType, new string[] { });
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText, string _defaultValue, ArgParse.ArgParseType _dataType)
+    {
+      init(_longName, _shortName, _required, _helpText, _defaultValue, _dataType, new string[] { });
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText, string _defaultValue, ArgParse.ArgParseType _dataType, string[] _validValues)
+    {
+      init(_longName, _shortName, _required, _helpText, _defaultValue, _dataType, _validValues);
+    }
+
+    private void init(string _longName, string _shortName, bool _required, string _helpText, string _defaultValue, ArgParse.ArgParseType _dataType, string[] _validValues)
+    {
+      longName = _longName;
+      shortName = _shortName;
+      helpText = _helpText;
+      defaultValue = _defaultValue;
+      required = _required;
+      argType = _dataType;
+      dataType = _dataType.ToType();
+      validValues = _validValues;
+    }
+  }
+
+  public static class ArgParseExtensions
+  {
+    public static Type ToType(this ArgParse.ArgParseType argType)
+    {
+      switch (argType)
+      {
+        case ArgParse.ArgParseType.DateTime:
+          return DateTime.MaxValue.GetType();
+        case ArgParse.ArgParseType.Int:
+          return int.MaxValue.GetType();
+        case ArgParse.ArgParseType.Long:
+          return long.MaxValue.GetType();
+        case ArgParse.ArgParseType.Boolean:
+          return true.GetType();
+        default:
+          return "".GetType();
+      }
+    }
+  }
+}
---
+++ OutlookSpy/Program.cs
@@ -0,0 +1,189 @@
+﻿using ArgumentParser;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookSpy
+{
+	class Program
+	{
+		static public Outlook.Application outlookApplication = new Outlook.Application();
+		static public Outlook.NameSpace outlookNamespace = outlookApplication.GetNamespace("MAPI");
+		static public Outlook.MAPIFolder inboxFolder = outlookNamespace.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+		static public Outlook.Accounts outlookAccounts = outlookApplication.Session.Accounts;
+    static public DataSet outlookDs = new DataSet("Outlook");
+    static public long maxMessages = 1000;
+
+    static void Main(string[] args)
+		{
+      ArgParse argparse = new ArgParse
+      (
+          new ArgItem("action", "a", true, "Action to be taken", "", ArgParse.ArgParseType.Choice, new string[] { "accounts", "full", "messages_meta", "message_single", "message_full" }),
+          new ArgItem("max-records", "m", false, "Number of messages to retrieve", "1000", ArgParse.ArgParseType.String),
+          new ArgItem("filter", "f", false, "Filter for searching email body", "", ArgParse.ArgParseType.String),
+          new ArgItem("subject-begins", "sb", false, "Filters messages if the subject begins with the specified value.", "", ArgParse.ArgParseType.String),
+          new ArgItem("subject-contains", "sc", false, "Filters messages if the subject contains the specified value.", "", ArgParse.ArgParseType.String),
+          new ArgItem("subject-equals", "se", false, "Filters messages if the subject equals the specified value.", "", ArgParse.ArgParseType.String),
+					new ArgItem("max-message-size", "fs", false, ".", "Restricts gathered messages to the specified size in bytes", ArgParse.ArgParseType.String),
+					new ArgItem("output", "o", false, "Output type: csv,json", "json", ArgParse.ArgParseType.Choice, new string[] { "csv", "json" }),
+          new ArgItem("url", "u", false, "url where data will be posted", "", ArgParse.ArgParseType.Url)
+      );
+
+			//argparse.parse(args);
+
+			string action = argparse.Get<string>("action");
+			int maxRecords = argparse.Get<int>("max-records");
+			string filter = argparse.Get<string>("filter");
+			string subjectBegins = argparse.Get<string>("subject-begins");
+			string subjectContains = argparse.Get<string>("subject-contains");
+			string subjectEquals = argparse.Get<string>("subject-equals");
+			long maxMessageSize = argparse.Get<long>("max-message-size");
+			string output = argparse.Get<string>("output");
+			string url = argparse.Get<string>("url");
+
+			Setup();
+      ListMailItems();
+      ListAccounts();
+    }
+
+		static void Setup()
+		{
+      DataTable messagesDt = new DataTable("messages");
+			messagesDt.Columns.Add("EntryId", Type.GetType("System.String"));
+			messagesDt.Columns.Add("DateReceived", Type.GetType("System.DateTime"));
+			messagesDt.Columns.Add("Subject", Type.GetType("System.String"));
+			messagesDt.Columns.Add("BodyText", Type.GetType("System.String"));
+			messagesDt.Columns.Add("Size", Type.GetType("System.Int64"));
+			outlookDs.Tables.Add(messagesDt);
+
+      DataTable accountDt = new DataTable("accounts");
+      accountDt.Columns.Add("SmtpAddress", Type.GetType("System.String"));
+      accountDt.Columns.Add("UserName", Type.GetType("System.String"));
+      accountDt.Columns.Add("DisplayName", Type.GetType("System.String"));
+      outlookDs.Tables.Add(accountDt);
+    }
+
+		static void ListAccounts()
+		{
+      DataTable accountDt = outlookDs.Tables["accounts"];
+
+      foreach (Outlook.Account account in outlookAccounts)
+			{
+        accountDt.Rows.Add(new object[] { account.SmtpAddress, account.UserName, account.DisplayName });
+				Console.WriteLine(string.Format("{0}\t{1}\t{2}", account.SmtpAddress, account.UserName, account.DisplayName));
+			}
+    }
+
+    static void ProcessFolder(Outlook.MAPIFolder folder)
+    {
+      DataTable messagesDt = outlookDs.Tables["messages"];
+      foreach (Outlook.MailItem message in folder.Items)
+      {
+        if(messagesDt.Rows.Count > 5)
+        {
+          break;
+        }
+        messagesDt.Rows.Add(new object[] { message.EntryID, message.ReceivedTime, message.Subject, message.Body, message.Size });
+      }
+
+      if(folder.Folders.Count > 0)
+      {
+        foreach (Outlook.MAPIFolder f in folder.Folders)
+        {
+          ProcessFolder(f);
+        }
+      }
+    }
+
+		static void ListMailItems()
+    {
+      foreach (Outlook.Folder f in inboxFolder.Folders)
+      {
+        Console.WriteLine(f.FolderPath);
+      }
+
+      ProcessFolder(inboxFolder);
+		}
+
+		private static string ToJSON(DataTable table)
+		{
+			//https://www.c-sharpcorner.com/UploadFile/9bff34/3-ways-to-convert-datatable-to-json-string-in-Asp-Net-C-Sharp/
+			var JSONString = new StringBuilder();
+			if (table.Rows.Count > 0)
+			{
+				JSONString.Append("[");
+				for (int i = 0; i < table.Rows.Count; i++)
+				{
+					JSONString.Append("{");
+					for (int j = 0; j < table.Columns.Count; j++)
+					{
+						if (j < table.Columns.Count - 1)
+						{
+							JSONString.Append("\"" + table.Columns[j].ColumnName.ToString().Replace("\"", "\\\"") + "\":" + "\"" + table.Rows[i][j].ToString().Replace("\"", "\\\"") + "\",");
+						}
+						else if (j == table.Columns.Count - 1)
+						{
+							JSONString.Append("\"" + table.Columns[j].ColumnName.ToString().Replace("\"", "\\\"") + "\":" + "\"" + table.Rows[i][j].ToString().Replace("\"", "\\\"") + "\"");
+						}
+					}
+					if (i == table.Rows.Count - 1)
+					{
+						JSONString.Append("}");
+					}
+					else
+					{
+						JSONString.Append("},");
+					}
+				}
+				JSONString.Append("]");
+			}
+			return JSONString.ToString();
+		}
+
+		public static string ToCSV(DataTable dt)
+		{
+			string result = "";
+			for (int i = 0; i < dt.Columns.Count; i++)
+			{
+				result += dt.Columns[i];
+				if (i < dt.Columns.Count - 1)
+				{
+					result += ",";
+				}
+			}
+
+			result += "\n";
+
+			foreach (DataRow dr in dt.Rows)
+			{
+				for (int i = 0; i < dt.Columns.Count; i++)
+				{
+					if (!Convert.IsDBNull(dr[i]))
+					{
+						string value = dr[i].ToString();
+						if (value.Contains(","))
+						{
+							value = String.Format("\"{0}\"", value.Replace("\"", "\\\""));
+							result += value;
+						}
+						else
+						{
+							result += dr[i].ToString();
+						}
+					}
+					if (i < dt.Columns.Count - 1)
+					{
+						result += ",";
+					}
+				}
+				result += "\n";
+			}
+
+			return result;
+		}
+	}
+}
